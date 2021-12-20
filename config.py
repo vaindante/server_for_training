336a23683328a0127b563418909cfb129c50b950
Vladislav <Vladislav.Grigorev@x5.ru>
2021-12-20 15:21:29 +0500

    add configuration options

--- config.py
+++ config.py
@@ -8,9 +8,12 @@ class Config:
     db_name = environ.var('library')
     db_host = environ.var('localhost')
     db_port = environ.var(5432)
+    database_url = environ.var('', name='DATABASE_URL')
 
     @property
     def db_url(self):
+        if self.database_url:
+            return self.database_url
         return f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
 
 
