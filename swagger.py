8cd87d3645e070d89be822f4f3a4005a38a48512
Vladislav <Vladislav.Grigorev@x5.ru>
2021-12-28 18:47:51 +0500

    fix import

--- swagger.py
+++ swagger.py
@@ -1,29 +1,8 @@
-from tkinter.scrolledtext import example
-
 tags = [
     {'name': 'User', 'description': 'Методы для работы с пользователями '},
     {'name': 'Books', 'description': 'Методы для взаимодействия с пользовательскими книгами'}
 ]
-import xml.etree.ElementTree as et
 
-# Создаем корневой элемент
-payee = et.Element('Users')
-user = et.SubElement(payee, 'item', attrib={'type': 'dict'})
-
-id_ = et.SubElement(user, 'id', attrib={'type': 'int'})
-id_.text = '1'
-email = et.SubElement(user, 'email', attrib={'type': 'str'})
-email.text = 'zoja_54@example.com'
-name = et.SubElement(user, 'name', attrib={'type': 'str'})
-name.text = 'Федосеева Анастасия Егоровна'
-created_at = et.SubElement(user, 'created_at', attrib={'type': 'str'})
-created_at.text = '2021-04-27 00:00:00'
-
-is_active = et.SubElement(user, 'is_active', attrib={'type': 'bool'})
-is_active.text = 'True'
-
-created_by = et.SubElement(user, 'created_by', attrib={'type': 'int'})
-created_by.text = '1'
 users_responses = {200: {
     "description": "Success",
     "content": {
